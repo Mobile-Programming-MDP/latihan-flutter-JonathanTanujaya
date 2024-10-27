81d6b2ace6134430b01c55e9c0f940d64e0db15b
Jonathan Tanujaya <jonathan.tanujaya.jt@gmail.com>
2024-10-27 08:22:51 +0700

    kelar

--- Latihan_UTS/latihan_uts/latihan_uts/lib/Latihan_Screen.dart
+++ Latihan_UTS/latihan_uts/latihan_uts/lib/Latihan_Screen.dart
@@ -9,159 +9,223 @@ class LatihanScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Latihan UTS"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Center(
-              child: Text(
-                "INFORMATIKA",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 21),
-              ),
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            Image.asset(
-              "images/latihan.jpg",
-              width: double.infinity,
-              height: 300,
-              fit: BoxFit.cover,
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            const Text(
-              "Universitas Multi Data Palembang",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 21),
-            ),
-            const Text(
-              "Kota Palembang, Prov. Sumatera Selatan ",
-              style: TextStyle(fontSize: 18),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: Colors.red[900],
-              ),
-              child: const Padding(
-                padding: EdgeInsets.all(16),
-                child: Column(
-                  children: [
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Status",
-                                style: TextStyle(color: Colors.white),
-                              ),
-                              Text(
-                                "Aktif",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              SizedBox(
-                                height: 8,
-                              ),
-                              Text(
-                                "Akreditasi",
-                                style: TextStyle(color: Colors.white),
-                              ),
-                              Text(
-                                "Unggul",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Tanggal Berdiri',
-                                style: TextStyle(color: Colors.white),
-                              ),
-                              Text(
-                                "9 April 2021",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              SizedBox(
-                                height: 8,
-                              ),
-                              Text(
-                                "Jumlah Mahasiswa",
-                                style: TextStyle(color: Colors.white),
-                              ),
-                              Text(
-                                "5000",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 8,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.phone,
-                              color: Colors.white,
-                            ),
-                            SizedBox(
-                              width: 4,
-                            ),
-                            Text(
-                              "0711-376400",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.mail,
-                              color: Colors.white,
-                            ),
-                            SizedBox(
-                              width: 4,
-                            ),
-                            Text(
-                              "kuliah@mdp.ac.id",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Center(
+                child: Text(
+                  "INFORMATIKA",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 21),
                 ),
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 8,
+              ),
+              Image.asset(
+                "images/latihan.jpg",
+                width: double.infinity,
+                height: 300,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              const Text(
+                "Universitas Multi Data Palembang",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 21),
+              ),
+              const Text(
+                "Kota Palembang, Prov. Sumatera Selatan ",
+                style: TextStyle(fontSize: 18),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.red[900],
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Status",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                Text(
+                                  "Aktif",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(
+                                  height: 8,
+                                ),
+                                Text(
+                                  "Akreditasi",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                Text(
+                                  "Unggul",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Tanggal Berdiri',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                Text(
+                                  "9 April 2021",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 8,
+                                ),
+                                Text(
+                                  "Jumlah Mahasiswa",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                Text(
+                                  "5000",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.phone,
+                                color: Colors.white,
+                              ),
+                              SizedBox(
+                                width: 4,
+                              ),
+                              Text(
+                                "0711-376400",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.mail,
+                                color: Colors.white,
+                              ),
+                              SizedBox(
+                                width: 4,
+                              ),
+                              Text(
+                                "kuliah@mdp.ac.id",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.link,
+                                color: Colors.white,
+                              ),
+                              SizedBox(
+                                width: 4,
+                              ),
+                              Text(
+                                "mdp.ac.id",
+                                style: TextStyle(color: Colors.white),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.grey[500],
+                ),
+                width: double.infinity,
+                child: Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Alamat",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      Text(
+                        "data",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.white),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: Colors.red[900],
+                        ),
+                        width: double.infinity,
+                        height: 50,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Lihat Map",
+                              style: TextStyle(color: Colors.white),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
